2324d15b73ceafa5509b453461ceea8697d407db
Hannes Bellmer <>
2017-04-10 21:06:15 +0200

    added history.sh and functions.sh from work branch

---
+++ profile.d/functions.sh
@@ -0,0 +1,61 @@
+function extract () {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xjf $1     ;;
+        *.tar.gz)    tar xzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xf $1      ;;
+        *.tbz2)      tar xjf $1     ;;
+        *.tgz)       tar xzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+function mkdircd () { mkdir "$@" && eval cd "\"\$$#\""; }
+
+# ssh wrapper that rename current tmux window to the hostname of the
+# remote host.
+function ssh() {
+    # Do nothing if we are not inside tmux or ssh is called without arguments
+    if [[ $# == 0 || -z $TMUX ]]; then
+        command ssh $@
+        return
+    fi
+    # The hostname is the last parameter 
+    local remote="${@: -1}"
+    local old_name="$(tmux display-message -p '#W')"
+    local renamed=0
+    # Save the current name
+    if [[ $remote != -* ]]; then
+        renamed=1
+        tmux rename-window $remote
+    fi
+    command ssh $@
+    sleep 2
+    if [[ $renamed == 1 ]]; then
+        tmux rename-window "$old_name"
+    fi
+}
+
+# SSH auto-completion based on entries in known_hosts.
+#if [[ -e ~/.ssh/known_hosts ]]; then
+#  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
+#fi
+
+
+# print the header (the first line of input)
+# # and then run the specified command on the body (the rest of the input)
+# # use it in a pipeline, e.g. ps | body grep somepattern
+body() {
+    IFS= read -r header
+    printf '%s\n' "$header"
+    "$@"
+}
